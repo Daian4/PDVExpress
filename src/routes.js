5b85e98dda51a1d114bb82fc9ff6c55c4ec82cd4
Daiana Menezes <daianalimaep@gmail.com>
2023-10-10 18:20:29 -0300

    criação de uma nova branch

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const routes = express();
 
 routes.get("/", (req, res) => {
-  return res.json("tudo certo");
+  return res.json("tudo certo!");
 });
 
 module.exports = routes;
