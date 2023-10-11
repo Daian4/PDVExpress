396644e900e78ab9ca8cc0a4ca8923755be85c16
Daiana <daianalimaep@gmail.com>
2023-10-11 16:17:14 -0300

    style: formatação do código

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,44 +1,44 @@
-const knex = require('../../database/connection')
-const bcrypt = require('bcrypt')
+const knex = require("../../database/connection");
+const bcrypt = require("bcrypt");
 // const jwt = require('jsonwebtoken')
 // const passwordJWT = require('')
 
 const registerUser = async (req, res) => {
-  const { nome, email, senha } = req.body
+  const { nome, email, senha } = req.body;
 
   if (!nome?.trim() || !email?.trim() || !senha?.trim()) {
     return res
       .status(400)
-      .json({ mensagem: 'Preencha todos os campos: nome, email e senha' })
+      .json({ mensagem: "Preencha todos os campos: nome, email e senha" });
   }
 
   try {
-    const passwordCrypt = await bcrypt.hash(senha, 10)
+    const passwordCrypt = await bcrypt.hash(senha, 10);
 
-    const user = await knex('usuarios').where('email', email).first()
+    const user = await knex("usuarios").where("email", email).first();
 
     if (user) {
-      return res.status(400).json({ mensagem: 'Email já existe' })
+      return res.status(400).json({ mensagem: "Email já existe" });
     }
 
-    const newUser = await knex('usuarios')
+    const newUser = await knex("usuarios")
       .insert({
         nome,
         email,
-        senha: passwordCrypt
+        senha: passwordCrypt,
       })
-      .returning('*')
+      .returning("*");
 
     if (!newUser) {
       return res
         .status(400)
-        .json({ mensagem: 'Não foi possível cadastrar o usuário' })
+        .json({ mensagem: "Não foi possível cadastrar o usuário" });
     }
 
-    return res.status(200).json(newUser)
+    return res.status(200).json(newUser);
   } catch (error) {
-    return res.status(500).json({ mensagem: error.message })
+    return res.status(500).json({ mensagem: error.message });
   }
-}
+};
 
-module.exports = { registerUser }
+module.exports = { registerUser };
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const routes = require("./routes");
 const app = express();
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
